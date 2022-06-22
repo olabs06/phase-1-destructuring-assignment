e56d638ce588aaa3c5d1279042318cafaa79d5db
Simeon <olabs06@yahoo.com>
2022-06-22 13:52:21 +0100

    modifying index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const farmAnimals = 'cow horse sheep pig chicken';
 
+let [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+
+const [bessie, ,dolly, babe, little] = farmAnimals.split(' ');
+
+let [blackAndWhite,  ,black, pink, ] = farmAnimals.split (' ');
+
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const  [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+const [r, o, y, g, b, ,v] = colors;
+
+const [indg] = ['indigo']
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +21,8 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const {muppetName, color, song, job, partner} = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +38,8 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const { album: {theMuppetMovie:{song2,song4}},nestedJob,nestedPartner} = nestedMuppet
 
 // Strings
 
@@ -46,3 +62,5 @@ const nestedMuppet = {
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+
